8e2918256a980de305b25803a868dd35b9965489
Florian Westphal <fw@strlen.de>
2007-07-25 22:26:02 +0000

    fix proto_tipc.h if AF_TIPC not present

--- proto_tipc.h
+++ proto_tipc.h
@@ -17,11 +17,10 @@ ssize_t tipc_write(int fd, const void *buf, size_t count);
 int tipc_accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 int tipc_listen(int sockfd, int);
 
-
 int tipc_socket_connect(void);
 #else
-static inline tipc_socket_bind(void) { return -1; }
-static inline tipc_socket_connect(void) { return -1; }
+static inline int tipc_socket_bind(void) { return -1; }
+static inline int tipc_socket_connect(void) { return -1; }
 #endif
 
 
