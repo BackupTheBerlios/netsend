7e97e6f5f99b8b8d74aa1b2dfcaf2a92e5a31bac
Florian Westphal <fw@strlen.de>
2008-03-22 19:20:00 +0000

    Removed splice() syscall() wrappers.

--- trans_common.c
+++ trans_common.c
@@ -23,31 +23,8 @@
 */
 
 #include "config.h"
-#ifdef HAVE_SPLICE
-#define SPLICE_F_MOVE 1
-#define SPLICE_F_MORE 4
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <sys/syscall.h>
-
-#undef __NR_sys_splice
-#ifndef __NR_splice
-# warning __NR_splice not defined - define as 313
-# define __NR_sys_splice 313
-#else
-# define __NR_sys_splice __NR_splice
-#endif
-
-
-static inline long splice(int fdin, loff_t *off_in, int fdout, loff_t *off_out,
-		                        size_t len, unsigned long flags)
-{
-	return syscall(__NR_sys_splice, fdin, off_in, fdout, off_out, len, flags);
-}
-#endif /* HAVE_SPLICE */
-
+#define _GNU_SOURCE
 #define _XOPEN_SOURCE 600	/* needed for posix_madvise/fadvise */
 #include <sys/mman.h>
 #include <fcntl.h>
