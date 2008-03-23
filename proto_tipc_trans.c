6f09dc5863ff88572d6b2f9e36cadde560c29b68
Florian Westphal <fw@strlen.de>
2008-03-23 20:24:44 +0000

    proto_tipc_trans: remove unneeded IP cruft

--- proto_tipc_trans.c
+++ proto_tipc_trans.c
@@ -32,7 +32,6 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <netinet/in.h>
 
 #include "debug.h"
 #include "global.h"
@@ -49,104 +48,14 @@ extern struct sock_callbacks sock_callbacks;
 */
 static int init_tipc_trans(void)
 {
-	bool use_multicast = false;
-	int fd = -1, ret;
-	struct addrinfo  hosthints, *hostres, *addrtmp;
-	struct protoent *protoent;
+	int fd;
 
-#ifdef HAVE_AF_TIPC
-	if (opts.family == AF_TIPC) {
-		fd = tipc_socket_connect();
-		if (fd < 0)
-			err_sys_die(EXIT_FAILNET, "tipc_socket_connect");
+	assert(opts.family == AF_TIPC);
 
-		return fd;
-	}
-#endif
-	memset(&hosthints, 0, sizeof(struct addrinfo));
-
-	/* probe our values */
-	hosthints.ai_family   = opts.family;
-	hosthints.ai_socktype = opts.socktype;
-	hosthints.ai_flags    = AI_ADDRCONFIG;
-
-	xgetaddrinfo(opts.hostname, opts.port, &hosthints, &hostres);
-
-	addrtmp = hostres;
-
-	for (addrtmp = hostres; addrtmp != NULL ; addrtmp = addrtmp->ai_next) {
-
-		if (opts.family != AF_UNSPEC &&
-			addrtmp->ai_family != opts.family) { /* user fixed family! */
-			continue;
-		}
-
-		fd = socket(addrtmp->ai_family, addrtmp->ai_socktype,
-				addrtmp->ai_protocol);
-		if (fd < 0) {
-			err_sys("socket");
-			continue;
-		}
-
-		protoent = getprotobynumber(addrtmp->ai_protocol);
-		if (protoent)
-			msg(LOUDISH, "socket created - protocol %s(%d)",
-				protoent->p_name, protoent->p_proto);
-
-		if (use_multicast) {
-			int hops_ttl = 30;
-				int on = 1;
-			switch (addrtmp->ai_family) {
-				case AF_INET6:
-					xsetsockopt(fd, IPPROTO_IPV6, IPV6_MULTICAST_HOPS, (char *)&hops_ttl,
-								sizeof(hops_ttl), "IPV6_MULTICAST_HOPS");
-					xsetsockopt(fd, IPPROTO_IPV6, IPV6_MULTICAST_LOOP,
-							&on, sizeof(int), "IPV6_MULTICAST_LOOP");
-					break;
-				case AF_INET:
-					xsetsockopt(fd, IPPROTO_IP, IP_MULTICAST_TTL,
-					         (char *)&hops_ttl, sizeof(hops_ttl), "IP_MULTICAST_TTL");
-
-					xsetsockopt(fd, IPPROTO_IP, IP_MULTICAST_LOOP,
-							&on, sizeof(int), "IP_MULTICAST_LOOP");
-					msg(STRESSFUL, "set IP_MULTICAST_LOOP option");
-					break;
-				default:
-					err_msg_die(EXIT_FAILINT, "Programmed Failure");
-			}
-		}
-
-		/* We iterate over our commandline argument array - where the user
-		** set socketoption and set this on our socket
-		** NOTE: it is necessary to set the soketoption before we call
-		** connect, which will invoke a syn packet!
-		** Example: if we set the receive buffer size to a greater value, tcp
-		** must handle this case and send in the initial packet a window scale
-		** option! Now you realize why we send the socketoption before we call
-		** connect.    --HGN
-		*/
-		set_socketopts(fd);
-
-		/* Connect to peer
-		** There are three advantages to call connect for all types
-		** of our socket protocols (especially udp)
-		**
-		** 1. We don't need to specify a destination address (only call write)
-		** 2. Performance advantages (kernel level)
-		** 3. Error detection (e.g. destination port unreachable at udp)
-		*/
-		ret = connect(fd, addrtmp->ai_addr, addrtmp->ai_addrlen);
-		if (ret == -1)
-			err_sys_die(EXIT_FAILNET, "Can't connect to %s", opts.hostname);
-
-		msg(LOUDISH, "socket connected to %s via port %s",
-			opts.hostname, opts.port);
-	}
-
+	fd = tipc_socket_connect();
 	if (fd < 0)
-		err_msg_die(EXIT_FAILNET, "No suitable socket found");
+		err_sys_die(EXIT_FAILNET, "tipc_socket_connect");
 
-	freeaddrinfo(hostres);
 	return fd;
 }
 
